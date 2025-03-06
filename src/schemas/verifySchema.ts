789d9d9c30d4fe7b09dbdfc3499f8275ee9a446d
Anas Nayeem <anasnayeem97@gmail.com>
2025-03-06 16:09:24 +0530

    OTP Verification done

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
-import { usernameSchema } from "./signupSchema";
 
 export const verifySchema = z.object({
-    username: usernameSchema,
     code: z.string().length(6, "Verification code must be six digits")
 })
